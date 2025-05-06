b7a48d0ed4548e53b755b493f2faa472f7801468
tsaimSD <102426677+tsaimSD@users.noreply.github.com>
2025-05-06 09:55:44 -0400

    Update run_hasher_property_test.tcl

--- run_hasher_property_test.tcl
+++ run_hasher_property_test.tcl
@@ -5,24 +5,19 @@ set rounds_list {2 3 4 5 6 7 8 9 10}
 # === Begin sweep ===
 foreach ghr $ghw_list {
   foreach rounds $rounds_list {
-
     puts "\n=== Running GHR_WIDTH=$ghr, ROUNDS=$rounds ==="
 
-    # Clear design state
-    remove_design -all -quiet
-    remove_file -all -quiet
-
-    # Setup design environment
+    # Set parameters
     set_parameter design_mode "rtl"
     set_parameter language_mode "sv"
     set_parameter define "GHR_WIDTH=$ghr,ROUNDS=$rounds"
 
-    # Read RTL and assertions
-    read_file -format sverilog complex_ghr_hasher.v
-    read_file -format sverilog complex_hashed_global_branch_predictor.v
-    read_file -format sverilog tb_global_predictor_checker.sv
+    # Read files (replacing any existing ones)
+    read_file -format sverilog -replace complex_ghr_hasher.v
+    read_file -format sverilog -replace complex_hashed_global_branch_predictor.v
+    read_file -format sverilog -replace tb_global_predictor_checker.sv
 
-    # Elaborate
+    # Re-elaborate
     elaborate -force
 
     # Run BMC
